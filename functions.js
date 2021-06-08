808b0badddb2c5ecef19841a8be3d88fa0c44859
Savannah Simonis <savannah.simonis@x-center.com>
2021-06-08 21:31:11 +0200

    Opdracht functions.js gemaakt

--- functions.js
+++ functions.js
@@ -14,20 +14,48 @@ console.log(greeting);
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
+function getSchool() {
+  return "Novi Hogeschool";
+}
 
+const novi = getSchool();
+console.log(novi);
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function compliment() {
+  return "Lekker bezig met die functies, Jan!";
+}
+
+const complimentje = compliment();
+console.log(complimentje);
 
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+function getGrades() {
+  return [8, 8.5, 6, 7];
+}
+
+const cijfers = getGrades();
+console.log(cijfers);
 
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails() {
+  const sav = {
+    firstName: "Savannah",
+    lastName: "Simonis"
+  };
+  return sav;
+}
+
+const arrayThing = getDetails();
+console.log(arrayThing);
